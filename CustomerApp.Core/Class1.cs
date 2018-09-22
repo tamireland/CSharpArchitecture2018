f1e5ef3ffb7b25f64b16941072d60e813152e81d
Tam Ireland <tamireland@hotmail.com>
2018-09-22 01:05:14 +0100

    Add project files.

---
+++ CustomerApp.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CustomerApp.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ CustomerApp.Core/DomainSevice/ICustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using CustomerApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerApp.Core.DomainSevice
+{
+    public interface ICustomerRepository
+    {
+        // -- CustomerRepository --
+        // Create Data
+        // No Id when enter, but Id when exist
+        Customer Create(Customer customer);
+
+        // Read Data
+        Customer Read(int id);
+        List<Customer> ReadAll();
+
+        // Update Data
+        Customer Update(Customer customerUpdate);
+
+        // Delete Data
+        Customer Delete(int id);
+    }
+}
